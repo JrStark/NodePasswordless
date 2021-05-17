69473bfdc00f54f7fab6d074f9f8b74b0f81e46f
JrStark <jerry.dumont@outlook.fr>
2021-05-17 18:07:02 +0200

    bdd instanciée en postgresql

--- doc & data/create_db.sql
+++ doc & data/create_db.sql
@@ -1,51 +1,52 @@
 BEGIN TRANSACTION;
 
 -- on commence par détruire toutes les tables si elles existent
-DROP TABLE IF EXISTS "post", "category", "users";
+DROP TABLE IF EXISTS "posts", "categories", "users", "roles";
 
 -- et c'est parti, on créé les tables
-CREATE TABLE "posts" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description_courte" TEXT,
-    "description_longue" TEXT,
-    "image" TEXT,
-    "enabled" INT NOT NULL DEFAULT "1",
-    "user_id"  INT REFERENCES users(id),
-    "category_id" INT NOT NULL REFERENCES category(id),
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ
-);
+
 
 CREATE TABLE "categories" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" TEXT UNIQUE NOT NULL,
-    "description" TEXT,
-    "image" TEXT,
-    "enabled" INT NOT NULL DEFAULT "1",
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ
-);
-
-CREATE TABLE "users" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "token" VARCHAR(64) COLLATE DEFAULT NULL,
-    "last_ping" DATETIME DEFAULT NULL,
-    "enabled" INT NOT NULL DEFAULT "1",
-    "role_id" INT NOT NULL REFERENCES role(id),
-    "created_at" TIMESTAMPTZ NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ NULL,
+    "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name" text UNIQUE NOT NULL,
+    "description" text,
+    "image" text,
+    "enabled" integer NOT NULL DEFAULT(1),
+    "created_at" timestamptz NOT NULL DEFAULT NOW(),
+    "updated_at" timestamptz
 );
 
 CREATE TABLE "roles" (
-    "id"  INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "level" INT NOT NULL,
-    "created_at" TIMESTAMPTZ NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMPTZ NULL,
+    "id"  integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "name" text NOT NULL,
+    "level" integer NOT NULL,
+    "created_at" timestamptz NULL DEFAULT NOW(),
+    "updated_at" timestamptz NULL
+);
+
+CREATE TABLE "users" (
+    "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "first_name" text NOT NULL,
+    "last_name" text NOT NULL,
+    "email" text NOT NULL,
+    "token" varchar(64) DEFAULT NULL,
+    "last_ping" date DEFAULT NULL,
+    "enabled" integer NOT NULL DEFAULT(1),
+    "role_id" integer NOT NULL REFERENCES roles(id),
+    "created_at" timestamptz NULL DEFAULT NOW(),
+    "updated_at" timestamptz NULL
+);
+
+CREATE TABLE "posts" (
+    "id" integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "title" text NOT NULL,
+    "description_courte" text,
+    "description_longue" text,
+    "image" text,
+    "user_id"  integer REFERENCES users(id),
+    "category_id" integer NOT NULL REFERENCES categories(id),
+    "created_at" timestamptz NOT NULL DEFAULT NOW(),
+    "updated_at" timestamptz
 );
 
 COMMIT;
--- doc & data/import_data.sql
+++ doc & data/import_data.sql
@@ -1,19 +1,17 @@
--- -----------------------------
--- table post
--- -----------------------------
-
-INSERT INTO "posts" ("title", "description_courte", "description_longue", "image", "enabled", "user_id", "category_id") VALUES
-('Un article pour tester', "une description courte de l'article", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste accusamus officia sunt voluptates esse unde delectus cupiditate quaerat. Voluptates saepe eligendi quisquam id, explicabo culpa itaque rerum neque expedita rem reprehenderit! Asperiores dicta, veritatis minus ea deleniti magni aspernatur porro, necessitatibus possimus vitae optio dignissimos natus exercitationem quaerat ipsam.", "/public/images/image1.jpg", 1, 1);
-
-INSERT INTO "categories" ("name", "description", "image", "enabled", "user_id", "category_id") VALUES
-('Categorie n°1', 'ceci est une description', 'public/images/images02.jpg', 1, 1, 1);
-
-INSERT INTO "users" ("first_name", "last_name", "email", "token", "enabled") VALUES
-('Toto', 'Lartichaut', 'hello.iters@gmail.com', 'e38ec985-a9e4-4ad6-9057-6667f5b9e12c', 1),
-('Tata', 'Banana', 'jerry.estelle.dumont@gmail.com', '94e3662ccd954ef9ba02d7a6e25704ab', 1);
+INSERT INTO "categories" ("name", "description", "image", "enabled") VALUES
+('Categorie n°1', 'ceci est une description', 'public/images/images02.jpg', 1);
 
 INSERT INTO "roles" ("name", "level") VALUES
 ('User', 1),
 ('Visitor', 0),
 ('Admin', 3),
 ('Author', 2);
+
+INSERT INTO "users" ("first_name", "last_name", "email", "token", "enabled", "role_id") VALUES
+('Toto', 'Lartichaut', 'hello.iters@gmail.com', 'e38ec985-a9e4-4ad6-9057-6667f5b9e12c', 1, 20),
+('Tata', 'Banana', 'jerry.estelle.dumont@gmail.com', '94e3662ccd954ef9ba02d7a6e25704ab', 1, 19);
+
+INSERT INTO "posts" ("title", "description_courte", "description_longue", "image", "user_id", "category_id") VALUES
+('Un article pour tester', 'Ceci est une description courte', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste accusamus officia sunt voluptates esse unde delectus cupiditate quaerat. Voluptates saepe eligendi quisquam id, explicabo culpa itaque rerum neque expedita rem reprehenderit! Asperiores dicta, veritatis minus ea deleniti magni aspernatur porro, necessitatibus possimus vitae optio dignissimos natus exercitationem quaerat ipsam.', '/public/images/image1.jpg', 18, 11),
+('Un second article pour tester', 'Ceci est une description courte', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto iste accusamus officia sunt voluptates esse unde delectus cupiditate quaerat. Voluptates saepe eligendi quisquam id, explicabo culpa itaque rerum neque expedita rem reprehenderit! Asperiores dicta, veritatis minus ea deleniti magni aspernatur porro, necessitatibus possimus vitae optio dignissimos natus exercitationem quaerat ipsam.', '/public/images/image1.jpg', 19, 11)
+;
